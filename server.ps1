08d73a2cb2c3cb32514fae608bf4d4bdbb94c38b
irkode <irkode@rikode.de>
2024-06-10 07:16:35 +0200

    simplified build scripts

--- server.ps1
+++ server.ps1
@@ -1,10 +1,1 @@
-param(
-   [string]$port = 1313,
-   [switch]$rm
-)
-
-if ($rm -and (Test-Path ./public)) { remove-item -Recurse ./public }
-
-
-write-Host "Server $port"
-& hugo server --cleanDestinationDir --gc --logLevel debug --disableFastRender --printPathWarnings --printUnusedTemplates -p $port
+& hugo server -D --logLevel debug --printPathWarnings --cleanDestinationDir --gc
